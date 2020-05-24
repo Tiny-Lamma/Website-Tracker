a2a35e0cd3e8b1fa38db19caede9ed06e210abbd
mark@cokers.com.au <mark@cokers.com.au>
2020-05-25 07:00:16 +1000

    adjust clear_history

--- clear_history.bat
+++ clear_history.bat
@@ -1,2 +1,4 @@
 del *.html
+echo off > cron.log
+echo on
 echo ID,URL,Latest Hash,Versions,Last Updated > site_db.csv
--- clear_history.sh
+++ clear_history.sh
@@ -1,2 +1,4 @@
 rm *.html
+rm cron.log
+touch cron.log
 echo ID,URL,Latest Hash,Versions,Last Updated > site_db.csv
